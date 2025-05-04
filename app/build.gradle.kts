116b5bd98466dc293e21e0cb3e26ca1c6f7e4d10
damhoe <damian.hoedtke@gmx.de>
2025-05-04 20:51:29 +0200

    Fix colors for some components

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.damhoe.skatscores"
-        compileSdk = 33
+        compileSdk = 34
         minSdk = 30
         targetSdk = 34
         versionCode = 1
@@ -54,17 +54,16 @@ android {
         resources.excludes.add("META-INF/*")
     }
 
-    compileSdk = 34
     buildToolsVersion = "34.0.0"
 }
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.compose.material3:material3:1.3.0")
+    implementation("androidx.compose.material3:material3:1.3.2")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment:2.8.3")
-    implementation("androidx.navigation:navigation-ui:2.8.3")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("androidx.navigation:navigation-fragment:2.8.9")
+    implementation("androidx.navigation:navigation-ui:2.8.9")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("com.android.support:support-annotations:28.0.0")
@@ -75,9 +74,9 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     // Livecycle
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.6")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
 
     // Dagger
     implementation("com.google.dagger:dagger:2.49")
@@ -88,7 +87,7 @@ dependencies {
     kapt("com.google.dagger:dagger-compiler:2.49")
 
     // Data store
-    implementation("androidx.datastore:datastore-preferences:1.1.1")
+    implementation("androidx.datastore:datastore-preferences:1.1.5")
 
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
